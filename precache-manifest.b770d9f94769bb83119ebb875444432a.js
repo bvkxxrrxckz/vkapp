f424ea312b4c543b4f428a5a0a6c21cbcf0acd89
bvkxxrrxckz <58874709+bvkxxrrxckz@users.noreply.github.com>
2019-12-14 16:34:48 +0700

    Updates

---
+++ precache-manifest.b770d9f94769bb83119ebb875444432a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d1bc2c2a6f5bba89a3055f7f9a1a32",
+    "url": "/vkapp/index.html"
+  },
+  {
+    "revision": "6817603b2e0c305611a2",
+    "url": "/vkapp/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "50cb7943a56320dba2fa",
+    "url": "/vkapp/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "6817603b2e0c305611a2",
+    "url": "/vkapp/static/js/2.03651fe2.chunk.js"
+  },
+  {
+    "revision": "50cb7943a56320dba2fa",
+    "url": "/vkapp/static/js/main.2c8b0366.chunk.js"
+  },
+  {
+    "revision": "fd911167741b072e040e",
+    "url": "/vkapp/static/js/runtime-main.45baba0c.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkapp/static/media/persik.4e1ec840.png"
+  }
+]);
